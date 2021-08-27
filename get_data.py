37cd4c058b0df9ba03d16de0d282f0bf90655d83
timfe <tim.fellerhoff@gmail.com>
2021-08-27 23:26:59 +0200

    minor update

--- get_data.py
+++ get_data.py
@@ -110,12 +110,3 @@ else:
     os.chdir(analysisDir + "/surface_tools")
     os.system("git pull")
     print("surface_tools already downloaded. Performed 'git pull'")
-
-#os.chdir(analysisDir+"/scripts")
-#if len(os.listdir(os.getcwd())) == 0:
-#    os.system("wget -N -L https://raw.githubusercontent.com/timfe/micaopen/master/MPC/scripts/01_constructSurfaces.sh >/dev/null 2>&1")
-#    os.system("wget -N -L https://raw.githubusercontent.com/timfe/micaopen/master/MPC/scripts/02_myelinMaptoSurf.sh >/dev/null 2>&1")
-#    os.system("wget -N -L https://raw.githubusercontent.com/timfe/micaopen/master/MPC/scripts/03_surf2mpc.m >/dev/null 2>&1")
-#    os.system("wget -N -L https://raw.githubusercontent.com/timfe/micaopen/master/MPC/scripts/build_mpc.m >/dev/null 2>&1")
-#else:
-#    print("Scripts already downloaded: " + os.getcwd())
