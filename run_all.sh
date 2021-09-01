7031240cf75a83fe0841c38db18dc72b347b8946
Tim F <tim.fellerhoff@gmail.com>
2021-09-01 18:38:24 +0200

    final fix

--- run_all.sh
+++ run_all.sh
@@ -2,6 +2,6 @@
 set -e # exit if any of the following commands fails
 source ~/myenv/bin/activate
 python get_data.py
-bash ~/project/CNG_cingulate_gradients/mpc_scripts/01_construcSurfaces.sh
+bash ~/project/CNG_cingulate_gradients/mpc_scripts/01_constructSurfaces.sh
 bash ~/project/CNG_cingulate_gradients/mpc_scripts/02_myelinMaptoSurf.sh
 python gather_analysis.py
