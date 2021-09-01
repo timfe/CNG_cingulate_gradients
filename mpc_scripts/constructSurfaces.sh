75298bbd4f6677931d016c5637ee153987e79d3c
Tim F <tim.fellerhoff@gmail.com>
2021-09-01 18:06:59 +0200

    gerions script

--- mpc_scripts/constructSurfaces.sh
+++ mpc_scripts/constructSurfaces.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 # activate freesurfer module
-#module load freesurfer/7.1
+module load freesurfer/7.1
 # add path to surface_tools
-#export PATH=$PATH:$HOME/project/hcp/analysis/surface_tools/equivolumetric_surfaces/
+export PATH=$PATH:$HOME/project/hcp/analysis/surface_tools/equivolumetric_surfaces/
 # set up as overearching bids directory
 dataDir=$HOME/project/hcp/data
 # change to your subject list
 input=$HOME/project/hcp/analysis/fulllist.txt
-while IFS= read -r sub; do
-    # Set up SUBJECTS_DIR for freesurfer based functions 
-    # In this setup the freesurfer output is nested within 'surfaces' 
+for sub in `cat "$input"`; do
+    # Set up SUBJECTS_DIR for freesurfer based functions
+    # In this setup the freesurfer output is nested within 'surfaces'
     # subdirectory of the subject's BIDS folder
     export SUBJECTS_DIR=${dataDir}/${sub}/surfaces/
 		# setup output directory
 		outDir=${dataDir}/${sub}/surfaces/equivSurfs
+		
 		# checks whether the output directory exists and makes it if it doesn't
 		[[ ! -d "$outDir" ]] && mkdir -p "$outDir"
 		# best practice to construct multiple sets of surfaces, with a range of surface numbers for optimisation
-		for num_surfs in 14;do
+		
+		for ((num_surfs = 14; num_surfs <= 14; num_surfs++)); do
 			# set output directory and create directory if necessary
 			[[ ! -d "$outDir"/"$num_surfs"surfs ]] && mkdir -p "$outDir"/"$num_surfs"surfs
 			for hemi in lh rh ; do
@@ -30,7 +32,5 @@ while IFS= read -r sub; do
 				--subject "$sub"
 				
 			done
-
 		done
-
-done < $input
+done
