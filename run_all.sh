303c9a6192924f6eaee36c4f8a9e57c1fcee3157
Tim F <tim.fellerhoff@gmail.com>
2021-09-01 19:38:03 +0200

    moved freesurfer load to run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e # exit if any of the following commands fails
 source ~/myenv/bin/activate
+module load freesurfer/7.1
 python get_data.py
 bash ~/project/CNG_cingulate_gradients/mpc_scripts/01_constructSurfaces.sh
 bash ~/project/CNG_cingulate_gradients/mpc_scripts/02_myelinMaptoSurf.sh
