bbf8d4420fdf988e2b26045aaf9b3d50c6851c4a
Tim F <tim.fellerhoff@gmail.com>
2021-09-01 18:19:41 +0200

    fix

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source myenv/bin/activate
+source ~/myenv/bin/activate
 python get_data.py
-bash /mpc_scripts/01_construcSurfaces.sh
-bash /mpc_scripts/02_myelinMaptoSurf.sh
+bash ~/project/CNG_cingulate_gradients/mpc_scripts/01_construcSurfaces.sh
+bash ~/project/CNG_cingulate_gradients/mpc_scripts/02_myelinMaptoSurf.sh
 python gather_analysis.py
